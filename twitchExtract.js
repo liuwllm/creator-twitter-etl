756167a772620a5472c01101333f242c924a91ff
liuwllm <liu.wllm@gmail.com>
2024-02-22 02:51:49 -0500

    Add sequential scraping for twitter links

--- twitchExtract.js
+++ twitchExtract.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import axios from 'axios';
+import * as puppeteer from 'puppeteer';
 
 import dotenv from 'dotenv';
 
@@ -10,7 +11,7 @@ async function getTwitchOAuth() {
     const config = {
         method: 'post',
         maxBodyLength: Infinity,
-        url: 'https://id.twitch.tv/oauth2/token?client_id=ho5ey7ej0cfvywswac5ivjpub0gk3h&client_secret=tqwy5g03byx5ub5w4jl3ecvd9xyjci&grant_type=client_credentials',
+        url: `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_ID}&client_secret=${process.env.TWITCH_SECRET}&grant_type=client_credentials`,
         headers: { }
     };
 
@@ -33,7 +34,7 @@ async function initializeCreators() {
     const config = {
         method: 'get',
         maxBodyLength: Infinity,
-        url: 'https://api.twitch.tv/helix/streams',
+        url: 'https://api.twitch.tv/helix/streams?first=100',
         headers: {
             'Content-Type' : 'application/json',
             'Client-ID': process.env.TWITCH_ID,
@@ -55,4 +56,36 @@ async function initializeCreators() {
         .catch((error) => console.log(error));
 }
 
-initializeCreators();
+async function findAllTwitter(creatorDb) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        ignoreHTTPSErrors: false,
+    })
+
+    let page = await browser.newPage();
+
+    for (const [key, value] of creatorDb.entries()) {
+        const twitchLink = `https://twitch.tv/${key}/about`;
+        await page.goto(twitchLink);
+
+        try {
+            await page.waitForSelector(
+                'a.ScCoreLink-sc-16kq0mq-0.dFpxxo.tw-link',
+                { timeout: 5000 }
+            );
+            const twitterLink = await page.$eval('a.ScCoreLink-sc-16kq0mq-0.dFpxxo.tw-link[href*=twitter]', tw => tw.href);
+            creatorDb.set(key, twitterLink);
+        }
+        catch (error) {
+            console.error("Timed out trying to retrieve Twitter link")
+        }
+    }
+
+    await page.close()
+}
+
+let creatorDb = await initializeCreators();
+await findAllTwitter(creatorDb);
+console.log(creatorDb);
+
+export { creatorDb }
